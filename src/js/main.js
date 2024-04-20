0b4d1f8456b88e8855b3cb7da6bd635461693181
Santiagopt97 <santiagopineda99@gmail.com>
2024-04-19 20:36:23 -0500

    se agregaron las funcionalidades del console

---
+++ src/js/main.js
@@ -0,0 +1,83 @@
+let userName = prompt("Enter your username") 
+let userLastName = prompt("Enter your lastname") 
+const age = prompt("Enter your age") 
+const userEmail = prompt("Enter your email") 
+let userPhone = parseInt( prompt("Enter your phone number"))
+let userAddress = prompt("Enter your Address") 
+
+/* Tres formas de escribir console.log */
+console.log("Name: " + userName)
+console.log("Name", userName)
+console.log(`Name: ${userName}`)
+
+console.log("Hello " + userName + " " + userLastName + ", you're " + age + ", your email is: " + userEmail + ", your phone is: " + userPhone + ", you live in " + userAddress)
+console.log("Hello", userName, userLastName, "you're", age, "your email is:", userEmail, "your phone is:", userPhone, "you live in", userAddress)
+
+console.log(`Hello ${userName} ${userLastName}!, 
+You're ${age}, 
+Your email is: ${userEmail}, 
+Your phone is: ${userPhone}, 
+You live in ${userAddress}`)
+
+/* 
+
+var: se usaba antes de la actualización de ECMA, ya no se utiliza, 
+let: puede sobreescribir su valor, 
+const: constante, no sobreescribe su valor
+
+*/
+
+/*
+
+console.log() General
+console.info() información, igual al general
+console.warn() advertencia -> naranjadito con !
+console.error() error -> rojo con x
+console.debug () msg para desarrollador, no sale en la consola propiamente, sino en el menu de mensajes -> debug
+console.clear() limpia la consola
+
+console.group ("Name del colapsable") -> Empieza a agrupar logs
+console.groupEnd() -> finaliza la agrupación de los logs
+
+- Podemos escribir css dentro de las comillas. Pj:
+
+console.log(`%c${userName}`, "background-color:black; color:white" )
+
+*/
+
+/* Mensajes para el usuario
+
+ alert(userAddress) */
+
+/* SweetAlert2 como opción para alertas estilizadas 
+
+let timerInterval;
+Swal.fire({
+    title: "Auto close alert!",
+    html: "I will close in <b></b> milliseconds.",
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: () => {
+        Swal.showLoading();
+        const timer = Swal.getPopup().querySelector("b");
+        timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+        }, 100);
+    },
+    willClose: () => {
+        clearInterval(timerInterval);
+    }
+}).then((result) => {
+    if (result.dismiss === Swal.DismissReason.timer) {
+        console.log("I was closed by the timer");
+    }
+});
+
+*/
+
+/* Arrays */
+
+const coders = ["Lupe Ortiz", "Mateo Duarte", "Maria Antonie"]
+
+console.log(coders)
+console.table(coders)
